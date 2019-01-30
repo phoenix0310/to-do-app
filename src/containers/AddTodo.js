af84a59e45e308d52a08a75f6c7b4cea3220e649
Trinh Tran Khanh Duy (323918) <ty323918@toyo-local.co.jp>
2019-01-30 17:31:08 +0700

    first commit

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions'
+
+const AddTodo = ({ dispatch }) => {
+  let input
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          if (!input.value.trim()) {
+              console.log(input.value.trim());
+            return
+          }
+          console.log(input.value.trim());
+          dispatch(addTodo(input.value))
+          input.value = ''
+        }}
+      >
+        <input ref={node => (input = node)} />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+export default connect()(AddTodo)
